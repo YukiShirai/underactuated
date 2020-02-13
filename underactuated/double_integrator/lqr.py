6d86c6f57ab0b2d7c52b13dfb35f8f487aa841d1
Russ Tedrake <russt@mit.edu>
2020-02-13 17:01:48 -0500

    updates for second dp lecture

--- underactuated/double_integrator/lqr.py
+++ underactuated/double_integrator/lqr.py
@@ -2,8 +2,8 @@ import numpy as np
 from pydrake.systems.controllers import LinearQuadraticRegulator
 
 # Define the double integrator's state space matrices.
-A = np.array([[0, 1], [0, 0]])
-B = np.array([[0], [1]])
+A = np.mat('0 1; 0 0')
+B = np.mat('0; 1')
 Q = np.eye(2)
 R = np.eye(1)
 
